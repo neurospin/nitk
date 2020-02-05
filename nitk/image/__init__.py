c26ddecb469b6e76b9875d3e0ab8ed85e0bc3db1
duchesnay <edouard.duchesnay@gmail.com>
2020-02-05 16:11:43 +0100

    img_brain_mask

--- nitk/image/__init__.py
+++ nitk/image/__init__.py
@@ -7,6 +7,8 @@ Created on Wed Feb  5 11:12:53 2020
 """
 
 from .img_to_array import img_to_array
+from .img_brain_mask import compute_brain_mask
 
 
-__all__ = ['img_to_array']
+__all__ = ['img_to_array',
+           'compute_brain_mask']
