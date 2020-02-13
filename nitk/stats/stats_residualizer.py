f5aa62ef4f7f67207b4e4d7c3e27fbfe7377f127
Edouard Duchesnay <edouard.duchesnay@cea.fr>
2020-02-13 16:36:44 +0100

    Residualizer

--- nitk/stats/stats_residualizer.py
+++ nitk/stats/stats_residualizer.py
@@ -3,105 +3,164 @@
 """
 Created on Thu Feb  6 15:15:14 2020
 
-@author: ed203246
-
-res = Residualizer(data, formula_res, formula_full=None)
-design_mat = res.get_design_mat()
-mod = ???
-
-for tr, te in cv.split(X, y):
-    Xtr ytr, design_mat_tr = X[tr, :], y[tr], design_mat[tr,: ]
-    Xte yte, design_mat_te = X[te, :], y[te], design_mat[te,: ]
-    Xtr = res.fit_transform(Xtr, design_mat_tr)
-    mod.fit(Xtr, ytr)
-    Xte = res.transform(Xte, design_mat_te)
-    mod.predict(Xte)
-
-
-
+@author: edouard.duchesnay@cea.fr
 """
 import numpy as np
 import mulm
 
+
 class Residualizer:
+    """
+    Residualization of a Y data on possibly adjusted for other variables.
+    If Y is a (n, p) array of p-dependant variables, we want to residualize for
+    "site" adjusted for "age + sex + diagnosis"
+
+    1) `Residualizer(data=df,
+                     formula_res="site",
+                     formula_full=site + age + sex + diagnosis")`
+    2) `get_design_mat()` will return the numpy (n, k) array design matrix.
+    Row selection can be done on both Y and design_mat (Cross-val., etc.)
+
+    3) `fit(Y, design_mat)` fits the model:
+    Y = b1 site + b2 age + b3 sex + b4 diagnosis + eps
+    => learn and store b1, b2, b3 and b4
+
+    4) `transform(Y, design_mat)` Y and design_mat can contains other
+    observations than the ones used in training phase.
+
+    Return Y - b1 site
+
+    Parameters
+    ----------
+    Y: array (n, p)
+        dependant variables
+
+    formula_res: str
+        Residualisation formula ex: "site"
+
+    formula_full: str
+        Full model (formula) of residualisation containing other variables
+        to adjust for. Ex.: "site + age + sex + diagnosis"
+
+    design_mat: array (n, k)
+        where Y.shape[0] == design_mat.shape[0] and design_mat.shape[1] is
+        the same in fit and transform
+
+    Returns
+    -------
+    Y: array (n, p)
+        Residualized dependant variables
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> import scipy.stats as stats
+    >>> from nitk.stats import Residualizer
+    >>> import seaborn as sns
+    >>> np.random.seed(1)
+    >>>
+    >>> # Dataset with site effect on age
+    >>> site = np.array([-1] * 50 + [1] * 50)
+    >>> age = np.random.uniform(10, 40, size=100) + 5 * site
+    >>> y = -0.1 * age  + site + np.random.normal(size=100)
+    >>> data = pd.DataFrame(dict(y=y, age=age, site=site.astype(object)))
+    >>>
+    >>> # Simple residualization on site
+    >>> res_spl = Residualizer(data, formula_res="site")
+    >>> yres = res_spl.fit_transform(y[:, None], res_spl.get_design_mat())
+    >>>
+    >>> # Site residualization adjusted for age
+    >>> res_adj = Residualizer(data, formula_res="site", formula_full="age + site")
+    >>> yadj = res_adj.fit_transform(y[:, None], res_adj.get_design_mat())
+    >>>
+    >>> # Site residualization adjusted for age provides higher correlation,
+    >>> # and lower stderr than simple residualization
+    >>> lm_res = stats.linregress(age, yres.ravel())
+    >>> lm_adj = stats.linregress(age, yadj.ravel())
+    >>>
+    >>> np.allclose((lm_res.slope, lm_res.rvalue, lm_res.stderr),
+    >>>             (-0.079187578, -0.623733003, 0.0100242219))
+    True
+    >>> np.allclose((lm_adj.slope, lm_adj.rvalue, lm_adj.stderr),
+    >>>             (-0.110779913, -0.7909219758, 0.00865778640))
+    True
+    """
+
     def __init__(self, data, formula_res, formula_full=None):
         if formula_full is None:
             formula_full = formula_res
         res_terms = mulm.design_matrix(formula=formula_res, data=data)[1].keys()
-        self.design_mat, self.t_contrasts, self.f_contrasts = mulm.design_matrix(formula=formula_full, data=data)
+        self.design_mat, self.t_contrasts, self.f_contrasts = \
+            mulm.design_matrix(formula=formula_full, data=data)
         # mask of terms in residualize formula within full model
-        self.mask = np.array([cont  for term, cont in self.t_contrasts.items() if term in res_terms]).sum(axis=0) == 1
+        self.mask = np.array([cont for term, cont in self.t_contrasts.items()
+                              if term in res_terms]).sum(axis=0) == 1
 
     def get_design_mat(self):
         return self.design_mat
 
     def fit(self, Y, design_mat):
+        """
+        Y: array (n, p)
+            Dependant variables
+
+        design_mat: array(n, k)
+            Design matrix of independant variables
+        """
+        assert Y.shape[0] == design_mat.shape[0]
+        assert self.mask.shape[0] == design_mat.shape[1]
         self.mod_mulm = mulm.MUOLS(Y, design_mat).fit()
         return self
 
-    def transform(self, Y):
-        return Y - np.dot(self.design_mat[:, self.mask], self.mod_mulm.coef[self.mask, :])
+    def transform(self, Y, design_mat):
+        assert Y.shape[0] == design_mat.shape[0]
+        assert self.mask.shape[0] == design_mat.shape[1]
+        return Y - np.dot(design_mat[:, self.mask],
+                          self.mod_mulm.coef[self.mask, :])
 
     def fit_transform(self, Y, design_mat):
         self.fit(Y, design_mat)
-        return self.transform(Y)
-
-def oldies_residualize(Y, formula_res, data, formula_full=None):
-    """
-    Residualisation of adjusted residualization.
-
-    Parameters
-    ----------
-    Y: array (n, p), dependant variables
-    formula_res: str, residualisation formula ex: "site":
-    1) Fit  Y = b0 + b1 site + eps
-    2) Return Y - b0 - b1 site
-    data: DataFrame of independant variables
-    formula_full:  str, full model formula (default None) ex: "age + sex + site + diagnosis". If not Null residualize
-    performs an adjusted residualization:
-    1) Fit Y = b1 age + b2 sex + b3 site + b4 diagnosis + eps
-    2) Return Y - b3 site
-
-    Returns
-    -------
-    Y: array (n, p), of residualized dependant variables
-    """
-    if formula_full is None:
-        formula_full = formula_res
-
-    res_terms = mulm.design_matrix(formula=formula_res, data=data)[1].keys()
-
-    X, t_contrasts, f_contrasts = mulm.design_matrix(formula=formula_full, data=data)
-
-    # Fit full model
-    mod_mulm = mulm.MUOLS(Y, X).fit()
-
-    # mask of terms in residualize formula within full model
-    mask = np.array([cont  for term, cont in t_contrasts.items() if term in res_terms]).sum(axis=0) == 1
-
-    return Y -  np.dot(X[:, mask], mod_mulm.coef[mask, :])
+        return self.transform(Y, design_mat)
 
 
 if __name__ == '__main__':
 
+    import numpy as np
     import pandas as pd
+    import scipy.stats as stats
+    from nitk.stats import Residualizer
     import seaborn as sns
+    np.random.seed(1)
 
     # Dataset with site effect on age
-    sex = np.random.choice([0, 1], 100)
     site = np.array([-1] * 50 + [1] * 50)
-    age = np.random.uniform(10, 40, size=100) + 10 * site
-    y = -0.1 * age + 0.0 * sex + site + np.random.normal(size=100)
-    data = pd.DataFrame(dict(y=y, age=age, sex=[str(s) for s in sex], site=[str(s) for s in sex]))
+    age = np.random.uniform(10, 40, size=100) + 5 * site
+    y = -0.1 * age + site + np.random.normal(size=100)
+    data = pd.DataFrame(dict(y=y, age=age, site=site.astype(object)))
 
+    # Simple residualization on site
     res_spl = Residualizer(data, formula_res="site")
-    data["yres"] = res_spl.fit_transform(y[:, None], res_spl.get_design_mat())
+    yres = res_spl.fit_transform(y[:, None], res_spl.get_design_mat())
 
-    res_adj = Residualizer(data, formula_res="site", formula_full="age + sex + site")
-    data["yadj"] = res_adj.fit_transform(y[:, None], res_adj.get_design_mat())
+    # Site residualization adjusted for age
+    res_adj = Residualizer(data, formula_res="site", formula_full="age + site")
+    yadj = res_adj.fit_transform(y[:, None], res_adj.get_design_mat())
 
-    # Requires adjusted residualization
+    # Site residualization adjusted for age provides higher correlation, and
+    # lower stderr than simple residualization
+    lm_res = stats.linregress(age, yres.ravel())
+    lm_adj = stats.linregress(age, yadj.ravel())
+
+    np.allclose((lm_res.slope, lm_res.rvalue, lm_res.stderr),
+                (-0.079187578, -0.623733003, 0.0100242219))
+
+    np.allclose((lm_adj.slope, lm_adj.rvalue, lm_adj.stderr),
+                (-0.110779913, -0.7909219758, 0.00865778640))
+
+    # Plot
+    data["yres"] = yres
+    data["yadj"] = yadj
     sns.lmplot("age", "y", hue="site", data=data)
     sns.lmplot("age", "yres", hue="site", data=data)
     sns.lmplot("age", "yadj", hue="site", data=data)
-
