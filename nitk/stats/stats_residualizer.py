f9cfee0b8df6951b8e8b5692a2a80cce0fdd10b3
EDOUARD DUCHESNAY <edouard.duchesnay@gmail.com>
2021-04-13 13:44:14 +0200

    stats_residualizer.py is deprecadetd

--- nitk/stats/stats_residualizer.py
+++ nitk/stats/stats_residualizer.py
@@ -7,13 +7,13 @@ Created on Thu Feb  6 15:15:14 2020
 """
 import numpy as np
 import mulm
-
+import warnings
 
 class Residualizer:
     """
     Residualization of a Y data on possibly adjusted for other variables.
-    If Y is a (n, p) array of p-dependant variables, we want to residualize for
-    "site" adjusted for "age + sex + diagnosis"
+    Example: Y is a (n, p) array of p-dependant variables, we want to residualize
+    for "site" adjusted for "age + sex + diagnosis"
 
     1) `Residualizer(data=df,
                      formula_res="site",
@@ -45,6 +45,8 @@ class Residualizer:
     design_mat: array (n, k)
         where Y.shape[0] == design_mat.shape[0] and design_mat.shape[1] is
         the same in fit and transform
+
+    pack_data: boolean (default=False)
 
     Returns
     -------
@@ -88,6 +90,10 @@ class Residualizer:
     """
 
     def __init__(self, data, formula_res, formula_full=None):
+        warnings.warn(
+            "nitk.stats.Residualizer is deprecated, use mulm.residualizer.Residualizer instead. See https://github.com/neurospin/pylearn-mulm",
+            DeprecationWarning
+        )
         if formula_full is None:
             formula_full = formula_res
         res_terms = mulm.design_matrix(formula=formula_res, data=data)[1].keys()
@@ -108,12 +114,14 @@ class Residualizer:
         design_mat: array(n, k)
             Design matrix of independant variables
         """
+
         assert Y.shape[0] == design_mat.shape[0]
         assert self.mask.shape[0] == design_mat.shape[1]
         self.mod_mulm = mulm.MUOLS(Y, design_mat).fit()
         return self
 
-    def transform(self, Y, design_mat):
+    def transform(self, Y, design_mat=None):
+
         assert Y.shape[0] == design_mat.shape[0]
         assert self.mask.shape[0] == design_mat.shape[1]
         return Y - np.dot(design_mat[:, self.mask],
@@ -122,6 +130,63 @@ class Residualizer:
     def fit_transform(self, Y, design_mat):
         self.fit(Y, design_mat)
         return self.transform(Y, design_mat)
+
+
+class ResidualizerEstimator:
+    """Wrap Residualizer into an Estimator compatible with sklearn API.
+
+    Parameters
+    ----------
+    residualizer: Residualizer
+    """
+
+    def __init__(self, residualizer):
+
+        self.residualizer = residualizer
+        self.design_mat_ncol = self.residualizer.design_mat.shape[1]
+
+    def fit(self, X, y=None):
+        design_mat, Y = self.upack(X)
+        return self.residualizer.fit(Y, design_mat)
+
+    def transform(self, X):
+        design_mat, Y = self.upack(X)
+        return self.residualizer.transform(Y, design_mat)
+
+    def fit_transform(self, X, y=None):
+        design_mat, Y = self.upack(X)
+        self.residualizer.fit(Y, design_mat)
+        return self.residualizer.transform(Y, design_mat)
+
+    def pack(self, Z, X):
+        """Pack (concat) Z (design matrix) and X to match scikit-learn pipelines.
+
+        Parameters
+        ----------
+        Z: array (n, k)
+            the design_matrix
+        X: array (n, p)
+            the input data for scikit-learn: fit(X, y) or transform(X)
+
+        Returns
+        -------
+        (n, (k+p)) array: [design_matrix, X]
+        """
+        return np.hstack([Z, X])
+
+    def upack(self, X):
+        """Unpack X and Z (design matrix) from X.
+
+        Parameters
+        ----------
+        X: array (n, (k+p))
+            array: [design_matrix, X]
+
+        Returns
+        -------
+            design_matrix, X
+        """
+        return X[:, :self.design_mat_ncol], X[:, self.design_mat_ncol:]
 
 
 def residualize(Y, data, formula_res, formula_full=None):
@@ -172,8 +237,19 @@ if __name__ == '__main__':
     sns.lmplot("age", "yres", hue="site", data=data)
     sns.lmplot("age", "yadj", hue="site", data=data)
 
-    # Usage combined with cross-validation
-    # Predict age from many ys
+    ############################################################################
+    # Rezidualizer as pre-processing of supervized prediction:
+    # Input: X = age + site + e, target = age
+    # Preprocessing:
+    # - Residualize X for "site" adjusted for "age"
+    # - Learn to predict age on residualized data
+    #
+    # Since age is used in residualization, it MUST be fitted on training data
+    # only.
+
+    # Dataset
+    site = np.array([-1] * 50 + [1] * 50)
+    age = np.random.uniform(10, 40, size=100) + 5 * site
     X = np.random.randn(100, 5)
     X[:, 0] = -0.1 * age + site + np.random.normal(size=100)
     X[:, 1] = -0.1 * age + site + np.random.normal(size=100)
@@ -183,21 +259,25 @@ if __name__ == '__main__':
     # X: input data of the predictive model are the
     # Predictive model
     from sklearn import linear_model
-    from sklearn.model_selection import StratifiedKFold
+    # from sklearn.model_selection import StratifiedKFold
+    from sklearn.model_selection import KFold
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.pipeline import make_pipeline
+    from sklearn.model_selection import cross_validate
     from sklearn import metrics
 
     lr = linear_model.Ridge(alpha=1)
-    cv = StratifiedKFold(n_splits=5, random_state=42)
+    scaler = StandardScaler()
+    cv = KFold(n_splits=5, random_state=42)
 
-    # Residializers
-    formula_res = 'site'
-    formula_full = 'site + age'
-    residualizer = Residualizer(data=demographic_df, formula_res=formula_res, formula_full=formula_full)
+    ############################################################################
+    # Usage 1: Manual slicing of train/test data: use Residualizer
 
+    residualizer = Residualizer(data=demographic_df, formula_res='site',
+                                formula_full='site + age')
     Z = residualizer.get_design_mat()
     scores = np.zeros((5, 2))
-
-    for i, (tr_idx, te_idx) in enumerate(cv.split(X, site)):
+    for i, (tr_idx, te_idx) in enumerate(cv.split(X, y)):
         X_tr, X_te = X[tr_idx, :], X[te_idx, :]
         Z_tr, Z_te = Z[tr_idx, :], Z[te_idx, :]
         y_tr, y_te = y[tr_idx], y[te_idx]
@@ -208,6 +288,9 @@ if __name__ == '__main__':
         # 2) Residualize
         X_res_tr = residualizer.transform(X_tr, Z_tr)
         X_res_te = residualizer.transform(X_te, Z_te)
+
+        X_res_tr = scaler.fit_transform(X_res_tr)
+        X_res_te = scaler.transform(X_res_te)
 
         # 3) Fit predictor on train residualized data
         lr.fit(X_res_tr, y_tr)
@@ -223,5 +306,24 @@ if __name__ == '__main__':
 
     print("Mean scores")
     print(scores.mean(axis=0))
-    print("Standard Errors")
-    print(scores.std(axis=0) / np.sqrt(5))
+
+    ############################################################################
+    # Usage 2: Usage with sklearn pipeline, cross_validate:
+    # use ResidualizerEstimator
+
+    residualizer = Residualizer(data=demographic_df, formula_res='site',
+                                formula_full='site + age')
+    # Extract design matrix and pack it with X
+    Z = residualizer.get_design_mat()
+
+    residualizer_wrapper = ResidualizerEstimator(residualizer)
+    ZX = residualizer_wrapper.pack(Z, X)
+
+    pipeline = make_pipeline(residualizer_wrapper, StandardScaler(), lr)
+    cv_res = cross_validate(estimator=pipeline, X=ZX, y=y, cv=cv, n_jobs=5,
+                            scoring=['r2', 'neg_mean_absolute_error'])
+
+    r2 = cv_res['test_r2'].mean()
+    mae = np.mean(-cv_res['test_neg_mean_absolute_error'])
+    print("CV R2:%.4f, MAE:%.4f" % (r2, mae))
+    assert np.allclose(scores.mean(axis=0).values, np.array([r2, mae]))
